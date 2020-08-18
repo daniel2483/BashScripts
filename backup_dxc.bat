6669b08ca716d66471da57bec49a21cd75fda5dd
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-08-18 10:05:23 -0600

    Adding backup script
    - This script is to backup important data from pc

---
+++ backup_dxc.bat
@@ -0,0 +1,15 @@
+robocopy.exe C:\Users\jrodrigue253\Desktop\DXC "D:\jrodrigue253" /e /z /r:0
+
+robocopy.exe C:\Users\jrodrigue253\Desktop\Books "D:\jrodrigue253" /e /z /r:0
+
+robocopy.exe C:\Users\jrodrigue253\Desktop\OCRs "D:\jrodrigue253" /e /z /r:0
+
+robocopy.exe C:\Users\jrodrigue253\Desktop\NSSR "D:\jrodrigue253" /e /z /r:0
+
+robocopy.exe C:\Users\jrodrigue253\Desktop\python "D:\jrodrigue253" /e /z /r:0
+
+robocopy.exe C:\Users\jrodrigue253\Desktop\CVs "D:\jrodrigue253" /e /z /r:0
+
+robocopy.exe C:\Users\jrodrigue253\Desktop\Bat "D:\jrodrigue253" /e /z /r:0
+
+pause
